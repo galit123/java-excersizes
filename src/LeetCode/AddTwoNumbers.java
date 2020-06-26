cbf4d62cb941bd2dca6fa1ed8d48e5314ec3dede
Galit H <galithayat@gmail.com>
2020-06-26 23:53:16 +0300

    Add two number - working solution

--- src/LeetCode/AddTwoNumbers.java
+++ src/LeetCode/AddTwoNumbers.java
@@ -3,52 +3,120 @@ package LeetCode;
 
 // Definition for singly-linked list.
 
-class Solution {
-    public class ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-    }
+class AddTwoNumbers {
+
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+//        ListNode l3 = new ListNode();
+//        ListNode result = l3;
+//
+//        int carry = 0;
+//        while (l1 != null && l2 != null){
+//            int res = l1.val + l2.val + carry;
+//            l3.val = res % 10;
+//            carry = res /10;
+//
+//            l1 = l1.next;
+//            l2 = l2.next;
+//
+//            if ((l1 != null) || (l2 != null) || carry != 0){
+//                l3.next = new ListNode(carry);
+//                l3 = l3.next;
+//            }
+//        }
+//
+//        while (l1 != null){
+//            l3.val += l1.val + carry;
+//            carry = l3.val /10;
+//            l3.val = l3.val % 10;
+//            l1 = l1.next;
+//            if (l1 != null){
+//                l3.next = new ListNode(carry);
+//                l3 = l3.next;
+//            }
+//        }
+//
+//
+//        while (l2 != null){
+//            l3.val += l2.val + carry;
+//            carry = l3.val /10;
+//            l3.val = l3.val % 10;
+//            l2 = l2.next;
+//            if (l2 != null){
+//                l3.next = new ListNode(carry);
+//                l3 = l3.next;
+//            }
+//        }
+//        return  result;
+//    }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l3 = new ListNode();
+        ListNode result = l3;
 
-        int left = 0;
-        ListNode cur = new ListNode();
-        ListNode head = cur;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int val1 = (l1 == null? 0 : l1.val);
+            int val2 = (l2 == null? 0 : l2.val);
+            int res = val1 + val2 + carry;
+            l3.val = res % 10;
+            carry = res / 10;
 
-        while ((l1 != null) && (l2 != null)){
-
-            int sum = l1.val + l2.val + left;
-            if (sum >= 10){
-                cur.val = sum % 10;
-                left = 1;
-            } else {
-                cur.val = sum;
-                left = 0;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
             }
 
-            l1 = l1.next;
-            l2 = l2.next;
-
-            if ((l1 != null) || (l2 != null)){
-                cur.next = new ListNode();
-                cur = cur.next;
+            if ((l1 != null) || (l2 != null) || carry != 0) {
+                l3.next = new ListNode(carry);
+                l3 = l3.next;
             }
         }
-
-        if (l1 != null){
-            cur.next = l1;
-        }
-
-        if (l2 != null){
-            cur.next = l2;
-        }
-        return head;
+        return result;
     }
 
     public static void main(String[] args) {
+        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
 
+        // 342 + 465
+        int[] ar1 = {2, 4, 3};
+        int[] ar2 = {5, 6, 4};
+        addTwoNumbers.addNumbers(ar1, ar2);
+
+        // 704 + 24
+        int[] ar3 = {4, 0, 7};
+        int[] ar4 = {4, 2};
+        addTwoNumbers.addNumbers(ar3, ar4);
+
+        int[] ar5 = {5};
+        addTwoNumbers.addNumbers(ar5, ar5);
+
+        int[] ar6 = {1};
+        int[] ar7 = {9, 9};
+        addTwoNumbers.addNumbers(ar6, ar7);
+
+    }
+
+    void addNumbers(int[] ar1, int[] ar2) {
+        ListNode l1 = createListNodeFromArray(ar1);
+        ListNode l2 = createListNodeFromArray(ar2);
+        ListNode res = addTwoNumbers(l1, l2);
+
+        System.out.println(l1 + " + " + l2 + " = " + res);
+
+    }
+
+    ListNode createListNodeFromArray(int[] ar) {
+        ListNode l1 = new ListNode();
+        ListNode res = l1;
+        int i = 0;
+        for (i = 0; i < ar.length - 1; i++) {
+            l1.val = ar[i];
+            l1.next = new ListNode();
+            l1 = l1.next;
+        }
+        l1.val = ar[i];
+        return res;
     }
 }
