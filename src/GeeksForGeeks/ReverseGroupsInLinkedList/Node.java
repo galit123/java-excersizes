19feb36cfdfcb299ce1fde9f566f0c073627d563
Galit H <galithayat@gmail.com>
2020-07-04 15:22:45 +0300

    reverse list in groups of k

---
+++ src/GeeksForGeeks/ReverseGroupsInLinkedList/Node.java
@@ -0,0 +1,13 @@
+package GeeksForGeeks.ReverseGroupsInLinkedList;
+
+public class Node {
+
+    int data;
+    Node next;
+
+    Node(int key) {
+        data = key;
+        next = null;
+    }
+
+}
---
+++ src/GeeksForGeeks/ReverseGroupsInLinkedList/ReverseGroupsInLinkedList.java
@@ -0,0 +1,88 @@
+package GeeksForGeeks.ReverseGroupsInLinkedList;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ReverseGroupsInLinkedList {
+    public Node reverse(Node node, int k) {
+        Queue<Node> q = new LinkedList<Node>();
+        Node cur = node;
+        while (cur != null) {
+            q.add(cur);
+            for (int i = 0; i < k && cur != null; i++) {
+                cur = cur.next;
+            }
+        }
+
+        cur = null;
+        Queue<Node> r = new LinkedList<Node>();
+        while (!q.isEmpty()) {
+            cur = q.peek();
+            r.add(reverseList(cur, 0, k));
+            q.remove(cur);
+        }
+
+        Node head = r.peek();
+        cur = head;
+        while (!r.isEmpty()) {
+            r.remove();
+            for (int i = 0; cur.next != null; i++) {
+                cur = cur.next;
+            }
+            cur.next = r.peek();
+            cur = cur.next;
+        }
+        return head;
+    }
+
+
+    private Node reverseList(Node node, int i, int k) {
+        if ((i >= k) || (node == null)) {
+            return null;
+        }
+        Node newNode = reverseList(node.next, i + 1, k);
+        if (node == null) {
+            return newNode;
+        }
+        node.next = null;
+        if (newNode == null) {
+            return node;
+        }
+        Node cur = newNode;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = node;
+
+        return newNode;
+    }
+
+    public static void main(String[] args) {
+        ReverseGroupsInLinkedList l = new ReverseGroupsInLinkedList();
+        int[] ar = {1, 2, 2, 4, 5, 6, 7, 8};
+        l.doArray(ar, 4);
+
+        int[] b = {1, 2, 3, 4, 5};
+        l.doArray(b, 3);
+
+    }
+
+    private void doArray(int[] ar, int k) {
+        Node cur = new Node(ar[0]);
+        Node head = cur;
+        for (int i = 1; i < ar.length; i++) {
+            cur.next = new Node(ar[i]);
+            cur = cur.next;
+        }
+
+        Node res = reverse(head, k);
+        cur = res;
+        while (cur != null) {
+            System.out.print(cur.data + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+
+    }
+
+}
