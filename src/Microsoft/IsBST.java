f3bc49fa78d3ce39116da01dfef2414531b78cd8
Galit H <galithayat@gmail.com>
2020-07-18 21:47:58 +0300

    is binary search tree

---
+++ src/Microsoft/IsBST.java
@@ -0,0 +1,59 @@
+package Microsoft;
+
+import BinaryTree.ConstractBinaryTreeFromArray;
+import BinaryTree.Node;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class IsBST {
+    boolean isBST(Node root){
+        List<Integer> list = new LinkedList<>();
+        return isBST(root, list);
+    }
+
+    private boolean isBST(Node root, List<Integer> list) {
+        if (root == null){
+            return true;
+        }
+        if (isBST(root.left, list)) {
+
+            if (!list.isEmpty() && list.get(list.size() - 1) >= root.value){
+                return false;
+            }
+            list.add(root.value);
+
+        }
+        return isBST(root.right, list);
+    }
+
+    public static void main(String[] args) {
+//           1
+//        /     \
+//       1       1
+//      / \     / \
+//     1   1   N   1
+//    / \ / \     / \
+//   1  1 1 1    N   N
+        ConstractBinaryTreeFromArray c = new ConstractBinaryTreeFromArray();
+        int[] a = {1, 1, 1, 1, 1, 'N', 1, 1, 1, 1, 1};
+        Node t = c.constractBinaryTree(a);
+        IsBST ibst = new IsBST();
+//        boolean x = ibst.isBST(t);
+//        System.out.println(x);
+
+//                   3
+//                  / \
+//                 2   5
+//               /  \
+//              1    4
+        int[] b = {3, 2, 5, 1, 4};
+        t = c.constractBinaryTree(b);
+        boolean y = ibst.isBST(t);
+        System.out.println(y);
+
+    }
+
+}
